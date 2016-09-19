87a81853d0faba928bdfa9028234e5f465402eda
Sacha Durand Saint Omer <sachadso@gmail.com>
2016-09-19 12:50:35 +0200

    Adds tvOS target

---
+++ SteviatvOS/SteviatvOS.h
@@ -0,0 +1,19 @@
+//
+//  SteviatvOS.h
+//  SteviatvOS
+//
+//  Created by Sacha Durand Saint Omer on 19/09/16.
+//  Copyright © 2016 Sacha Durand Saint Omer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SteviatvOS.
+FOUNDATION_EXPORT double SteviatvOSVersionNumber;
+
+//! Project version string for SteviatvOS.
+FOUNDATION_EXPORT const unsigned char SteviatvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SteviatvOS/PublicHeader.h>
+
+
